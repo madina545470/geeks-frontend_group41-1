811a5125deabda2bfde90a8b0e15a7e2a985ff39
madina545470 <matievamadina48@gmail.com>
2024-09-12 12:31:44 +0600

    Add files via upload

--- project/js/lesson.js
+++ project/js/lesson.js
@@ -8,13 +8,54 @@ const regExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
 
 
 phoneButton.onclick = () => {
-   if (regExp.test(phoneInput.value)) {
-       phoneSpan.innerHTML = `Correct phone number`
-       phoneSpan.style.color = `green`
-   }else{
-       phoneSpan.innerHTML = `Incorrect phone number`
-       phoneSpan.style.color = `red`
-   }
+    if (regExp.test(phoneInput.value)) {
+        phoneSpan.innerHTML = `Correct phone number`
+        phoneSpan.style.color = `green`
+    } else {
+        phoneSpan.innerHTML = `Incorrect phone number`
+        phoneSpan.style.color = `red`
+    }
 }
 
 
+//TAB SLIDER
+
+const tabContentBlocks = document.querySelectorAll(".tab_content_block");
+const tabContentItems = document.querySelectorAll(".tab_content_item");
+const tabContentItemsParent = document.querySelector(`.tab_content_items`);
+let index = 0
+const hideTabContent = () => {
+    tabContentBlocks.forEach(item => {
+        item.style.display = 'none';
+    })
+    tabContentItems.forEach(item => {
+        item.classList.remove(`tab_content_item_active`)
+    })
+}
+
+const showTabContent = (index = 0) => {
+    tabContentBlocks[index].style.display = 'block';
+    tabContentItems[index].classList.add('tab_content_item_active');
+}
+
+
+hideTabContent()
+showTabContent(index)
+
+tabContentItemsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabContentItems.forEach((item, tabIndex) => {
+            if (event.target === item) {
+                hideTabContent()
+                index = tabIndex
+                showTabContent(index)
+            }
+        })
+    }
+}
+const autoSlider = () => {
+    hideTabContent()
+    index = (index + 1) % tabContentItems.length
+    showTabContent(index)
+}
+setInterval(autoSlider, 3000)
