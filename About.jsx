6c02ec50927c11b56a43af0ecdb039fed921f4e9
madina545470 <matievamadina48@gmail.com>
2024-10-07 19:13:59 +0600

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = ({ info }) => {
+    return (
+        <div>
+            <h2>{info.title}</h2>
+            <p>{info.body}</p>
+        </div>
+    );
+};
+
+export default About;
